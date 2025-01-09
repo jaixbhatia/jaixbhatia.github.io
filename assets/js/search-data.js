6975e0d152e00a25b1b551290136e1525faf0ba1
jaixbhatia <66648216+jaixbhatia@users.noreply.github.com>
2025-01-09 06:57:53 +0000

    Deploying to gh-pages from @ jaixbhatia/victoria-delaney@6bef63ce4b54b244b8d4b91298de3e4c7761b7a5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,21 +44,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/lab/";
           },
-        },{id: "dropdown-publications",
-              title: "publications",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "";
-              },
-            },{id: "dropdown-projects",
-              title: "projects",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "";
-              },
-            },{id: "news-a-simple-inline-announcement",
+        },{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
           section: "News",},{id: "news-a-long-announcement-with-details",
